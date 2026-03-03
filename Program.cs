2dd3ac0a2fe51c11e8fc7443d6519b17db640d18
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-03-02 16:14:21 -0800

    I have started by doing all the public string, int, bool, and double. so far everything is going good.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Final_Project_Intake_Management_system
+﻿using System;
+using System.Collections.Generic;
+namespace Final_Project_Intake_Management_system
 {
+    class Applicant
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Phone;
+        public string City;
+        public string postiionApplied;
+        public string EducationLevel;
+
+        public int Age;
+        public int YearsExperience;
+        public int ExpectedSalary;
+        public double TechnicalScore;
+        public double CommunicatinScore;
+        public double OverallScore;
+        public int ApplicationID;
+
+        public bool HasDegree;
+        public bool IsWillingToRelocate;
+        public bool HasCertifications;
+        public bool EligibleForInterview;
+
+        public int PositionChoice;
+        public int EmploymentTypeChoice;
+        public string EmploymentType;
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
